42712ffbb17d8ec96a97d67036815c4e885a17c1
asoilihi <lhabdou26@hotmail.fr>
2018-09-13 11:38:26 +0200

    aso -initialisation du projet spring kamusi

---
+++ kamusi/src/main/java/com/lh/kamusi/KamusiApplication.java
@@ -0,0 +1,12 @@
+package com.lh.kamusi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamusiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamusiApplication.class, args);
+	}
+}
